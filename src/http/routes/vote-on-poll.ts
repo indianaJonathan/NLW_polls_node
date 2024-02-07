5925f0e0fe6d58aa161ccce825a2ed4861f49974
Jonathan Hermam <jherman@outlook.com.br>
2024-02-07 19:42:41 -0300

    Added websocket pub/sub strategies for votes score

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -3,6 +3,7 @@ import { prisma } from "../../lib/prisma";
 import { randomUUID } from "node:crypto";
 import { FastifyInstance } from "fastify";
 import { redis } from "../../lib/redis";
+import { voting } from "../../utils/voting-pub-sub";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -39,7 +40,12 @@ export async function voteOnPoll(app: FastifyInstance) {
           }
         });
 
-        await redis.zincrby(pollId, -1, userPrevVotedOnPoll.pollOptionId);
+        const votes = await redis.zincrby(pollId, -1, userPrevVotedOnPoll.pollOptionId);
+
+        voting.publish(pollId, {
+          pollOptionId: userPrevVotedOnPoll.pollOptionId, 
+          votes: Number(votes),
+        });
 
       } else if (userPrevVotedOnPoll) {
         return reply.status(400).send({ message: "You've already voted on this poll" });
@@ -65,7 +71,12 @@ export async function voteOnPoll(app: FastifyInstance) {
       }
     });
 
-    await redis.zincrby(pollId, 1, pollOptionId);
+    const votes = await redis.zincrby(pollId, 1, pollOptionId);
+
+    voting.publish(pollId, {
+      pollOptionId, 
+      votes: Number(votes),
+    });
 
     return reply.status(201).send();
   });
